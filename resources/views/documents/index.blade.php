f1b636fd7a1c23742bca237662f05296f36d4d3e
Jérôme Combes <jerome@planningbiblio.fr>
2025-05-30 22:36:38 +0200

    Remove fieldsets behind DataTables

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -8,62 +8,57 @@ Note that only pdf, jpg and word documents (with extensions .pdf, jpg our .jpeg 
 <br/>
 </p>
 
-<fieldset>
+  @if($documents->count())
+      <table class='datatable' data-sort='[]'>
+          <thead>
+              <tr>
+                  @if(Auth::user()->admin and !session('student'))
+                      <th>Student</th>
+                  @endif
+                  <th>Name</th>
+                  <th>Documents</th>
+                  @if(Auth::user()->admin)
+                      <th>Visibility</th>
+                  @endif
+                  <th>Type</th>
+                  <th>Size</th>
+                  <th>Date</th>
+              </tr>
+          </thead>
+          <tbody>
 
-    @if($documents->count())
-        <table class='datatable' data-sort='[]'>
-            <thead>
-                <tr>
-                    @if(Auth::user()->admin and !session('student'))
-                        <th>Student</th>
-                    @endif
-                    <th>Name</th>
-                    <th>Documents</th>
-                    @if(Auth::user()->admin)
-                        <th>Visibility</th>
-                    @endif
-                    <th>Type</th>
-                    <th>Size</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
+          @foreach($documents as $doc)
+              <tr>
+                  @if(Auth::user()->admin and !session('student'))
+                      <td>{{ $doc->firstname }} {{ $doc->lastname }}</td>
+                  @endif
+                  <td><a href='/show/{{ $doc->id }}' target='_blank'> {{ $doc->name }}</a></td>
+                  <td>{{ $doc->rel }}</td>
+                  @if(Auth::user()->admin)
+                      <td>{{ $doc->visibility }}</td>
+                  @endif
+                  <td>{{ $doc->type }}</td>
+                  <td style='white-space:nowrap; text-align:right;'>{{ $doc->size }}</td>
+                  <td style='white-space:nowrap;'>{{ $doc->time }}</td>
+              </tr>
+          @endforeach
 
-            @foreach($documents as $doc)
-                <tr>
-                    @if(Auth::user()->admin and !session('student'))
-                        <td>{{ $doc->firstname }} {{ $doc->lastname }}</td>
-                    @endif
-                    <td><a href='/show/{{ $doc->id }}' target='_blank'> {{ $doc->name }}</a></td>
-                    <td>{{ $doc->rel }}</td>
-                    @if(Auth::user()->admin)
-                        <td>{{ $doc->visibility }}</td>
-                    @endif
-                    <td>{{ $doc->type }}</td>
-                    <td style='white-space:nowrap; text-align:right;'>{{ $doc->size }}</td>
-                    <td style='white-space:nowrap;'>{{ $doc->time }}</td>
-                </tr>
-            @endforeach
+          </tbody>
+      </table>
 
-            </tbody>
-        </table>
+  @else
+      No document
+  @endif
 
-    @else
-        No document
-    @endif
-
-    <p class='align-right'>
-        @if(Auth::user()->admin)
-            @if(session('student'))
-                <a class='myUI-button' href='/documents/edit'>Edit</a>
-                <a class='myUI-button' href='/documents/add'>Add files</a>
-            @endif
-        @else
-            <a class='myUI-button' href='/documents/add'>Add files</a>
-        @endif
-    </p>
-
-</fieldset>
-
+  <p class='align-right'>
+      @if(Auth::user()->admin)
+          @if(session('student'))
+              <a class='myUI-button' href='/documents/edit'>Edit</a>
+              <a class='myUI-button' href='/documents/add'>Add files</a>
+          @endif
+      @else
+          <a class='myUI-button' href='/documents/add'>Add files</a>
+      @endif
+  </p>
 
 @endsection
--- resources/views/evaluations/table.blade.php
+++ resources/views/evaluations/table.blade.php
@@ -8,34 +8,30 @@
     <input type='button' value='Export to Excel' class='btn btn-primary' onclick='$(".buttons-excel").click();'/>
   </div>
 
-  <fieldset>
-
-    @if (empty($data))
-      <h4>No evaluation found !</h4>
-    @else
-      <table class='datatable' data-sort='[[0,"asc"]]'>
-        <thead>
-          <tr>
-            @foreach ($questions as $question)
-              <th>{{ $question }}</th>
-            @endforeach
-          </tr>
-        </thead>
-
-        <tbody>
-          @foreach($data as $elem)
-            <tr>
-              @for ($i=1; $i < count($questions)+1; $i++)
-                <td>
-                  <div style='height:50px;overflow:hidden;' title='{{ $elem[$i] }}'>{{ $elem[$i] }}</div>
-                </td>
-              @endfor
-            </tr>
+  @if (empty($data))
+    <h4>No evaluation found !</h4>
+  @else
+    <table class='datatable' data-sort='[[0,"asc"]]'>
+      <thead>
+        <tr>
+          @foreach ($questions as $question)
+            <th>{{ $question }}</th>
           @endforeach
-        </tbody>
-      </table>
-    @endif
+        </tr>
+      </thead>
 
-  </fieldset>
+      <tbody>
+        @foreach($data as $elem)
+          <tr>
+            @for ($i=1; $i < count($questions)+1; $i++)
+              <td>
+                <div style='height:50px;overflow:hidden;' title='{{ $elem[$i] }}'>{{ $elem[$i] }}</div>
+              </td>
+            @endfor
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  @endif
 
 @endsection
